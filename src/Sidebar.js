240530da3f0ef6d005b42bf735713da485e77b4c
Jason Breedlove <jbreedlove@jasonbreedlove.dev>
2022-07-24 13:07:39 -0700

    import material-ui icons, begin on sidebar component

---
+++ src/Sidebar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Sidebar.styles.css";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import HomeIcon from "@mui/icons-material/Home";
+import TagIcon from "@mui/icons-material/Tag";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import EmailIcon from "@mui/icons-material/Email";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+import ListIcon from "@mui/icons-material/List";
+import PersonIcon from "@mui/icons-material/Person";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+
+function Sidebar(props) {
+  return <div className={"sidebar"}></div>;
+}
+
+export default Sidebar;
